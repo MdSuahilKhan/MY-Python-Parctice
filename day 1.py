972abe2b962ec5e362f2cb750640db8cd938eccf
MdSuahilKhan <suhailkhancse07@gmail.com>
2026-08-23 19:36:50 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,90 @@
+"""
+1
+print('hello python)
+
+2     
+name = input()
+print ('suhail khan',name)
+
+3
+
+number = input()
+print (number)
+
+4
+
+
+num1 = input('Enter the first numbar')
+num2 = input('Enter the second number')
+print (num1)
+print (num2)
+
+5
+
+
+name = input('My name is')
+age = input ('My age is')
+
+print(name)
+print (age)
+
+6
+
+number = int ( input (' enter the integer'))
+print(' find the squre',number * number)
+
+7
+
+sum1 = int ( input ( 'enter the first number'))
+sum2 = int ( input ( 'enter the second number'))
+print ( 'total sum',sum1 + sum2)
+
+8
+
+a = int ( input('enter the first number'))
+b = int ( input('enter the second number'))
+print ('Addition', a+b)
+print ('subtraction', a-b)
+print ('Multiplication ', a*b)
+
+9
+
+number= int(input('enter the number'))
+print (' the entred',number)
+
+10
+
+number=int(input('enter an integer'))
+print('Double', number*2)
+
+11
+
+number=float(input('enter the float number'))
+print(number)
+
+
+
+num1 = float(input("enter the first float"))
+num2 = float(input("enter the second fload "))
+print("sum = ", num1 + num2)
+
+"""
+number = float(input('enter the float'))
+print('the haft', number/2)
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day2.py
@@ -0,0 +1,80 @@
+
+
+"""
+a=5
+b=10
+print(a<b<20)
+
+
+x=True
+y=False
+print(x+y*x)
+
+print(4**0**2)
+
+a=12
+b=5
+print(a^b)
+
+print((3 and 0) or (0 
+
+
+a=7
+print (~a+1)
+
+
+a=True
+b=False
+print (( a or b )and not (a and b))
+
+
+print(2+3*4**2//8%3)
+
+print(1<<2+1)
+
+
+a=3
+b=2
+a*=b+1
+print(a)
+
+print(3<5>2==2)
+
+
+print(10//3*3+1%3)
+
+
+x=10
+y=20
+print(x & y | x^y)
+
+
+a= True
+b= False
+print(a&b | a^b)
+
+
+x=8
+print(x>>1<<2)
+
+
+
+print(5+True * False + not False)
+
+
+
+print((not 0 ) * (False or 1))
+
+
+
+
+print(4+3*2**2//2-1)
+
+"""
+
+
+a=True
+b=False
+print (( a or b )and not (a and b))
+
+
