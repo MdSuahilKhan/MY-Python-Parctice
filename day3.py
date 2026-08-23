98bf88b46b6814cd9e8c3d64f4ea413221dd7712
MdSuahilKhan <suhailkhancse07@gmail.com>
2026-08-23 19:36:15 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,106 @@
+
+"""
+
+Write a Python program to check if a number is positive. 
+
+num = int (input("Entre the number"))
+if num > 0:
+    print("postive")
+    
+
+ Print "Eligible to vote" if age is 18 or above.
+
+  age = int ( input("Entre the age"))
+ if age > 18:
+    print("Eligible to vote")
+
+
+Check if a number is divisible by 7. 
+
+
+num = int(input("entre the numbe"))
+if num % 7:
+    print("divisible by 7")
+
+
+  Print "Pass" if marks are greater than 40.
+
+  mark = int(input("entre the marks" ))
+if mark > 40:
+     print("pass")
+
+
+Check if a number is greater than 100.
+
+      
+num = int(input("entre the numbe"))
+if num >100:
+    print("greater than 100")
+
+Display a message if temperature exceeds 45°C.
+
+temp = float (input("entre the numbe"))
+if temp > 45:
+    print(" high temperature ")
+
+    
+
+Check if a string length is more than 8 characters.
+
+st = "suhailkhan"
+print(st)
+if len(st)>8:
+    print(" more than 8 characters")
+
+
+Print "Logged In" if password matches "admin123".
+
+password = input("entre the password")
+if password == "admin123":
+     print("logged in")
+
+Check if a number is a multiple of 10.
+
+num=int(input("entre the number"))
+if num % 10:
+    print("the multiple of 10 ")
+
+
+Print a warning if balance is below minimum limit.
+
+bal = float (input("entre the balance"))
+if bal < 1000:
+    print("if balance is below minimum limit")
+"""
+
+st = "suhailkhan"
+print(st)
+if len(st)>8:
+    print(" more than 8 characters")    
+             
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
